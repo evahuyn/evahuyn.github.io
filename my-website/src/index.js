e3f4ab47482b52b3c002290b7a822d19086290b2
Eva Hu <huyinuo@live.unc.edu>
2020-12-22 12:35:47 +0800

    setup my-website

---
+++ my-website/src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const Title = () =>{
+  return (
+    <h1>Advanced React</h1>
+  );
+}
+
+ReactDOM.render(
+  <Title />,
+  document.getElementById('root')
+);
+
