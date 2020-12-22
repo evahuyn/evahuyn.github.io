f56d86e09d38dca0d394b6608329dede304a4038
Eva Hu <huyinuo@live.unc.edu>
2020-12-23 00:29:56 +0800

    change navbar

--- my-website/src/navbar.js
+++ my-website/src/navbar.js
@@ -1,28 +1,48 @@
-import React from 'react'
+import React, {useState} from 'react'
 import $ from 'jquery'
 import {Link} from 'react-router-dom';
+import "./navbar.css"
+
+const navStyle = {
+  display: "flex",
+  alignItems: 'center',
+  justifyContent: 'center',
+  position: "fixed",
+  width: "100%",
+  transition: "500ms ease",
+}
+const preStyle = {
+  height: "12vh",
+  backgroundColor: "transparent",
+  borderBottom: "1px solid white",
+}
+
+const postStyle = {
+  backgroundColor: "rgb(255,255,255,0.8)",
+  height: "10vh",
+}
+
+// const useHover = () => {
+//   const [ hover, setHover ] = useState(false);
+
+//   const onMouseEnter = () => {
+//     setHover(true);
+//   };
+
+//   const onMouseLeave = () => {
+//     setHover(false);
+//   };
+
+//   let hoverStyle = !hover ? {
+//       color: "black", transition: 'all .2s ease-in-out',
+//     } : {
+//       color: "white", transition: 'all .2s ease-in-out',
+//     };
+
+//     return { hoverStyle, onMouseEnter, onMouseLeave };
+// };
 
 const Navbar = () =>{
-
-  const navStyle = {
-    display: "flex",
-    alignItems: 'center',
-    justifyContent: 'center',
-    position: "fixed",
-    width: "100%",
-    transition: "500ms ease",
-  }
-  const preStyle = {
-    height: "12vh",
-    backgroundColor: "transparent",
-    borderBottom: "1px solid white",
-  }
-
-  const postStyle = {
-    backgroundColor: "rgb(255,255,255,0.8)",
-    height: "10vh",
-  }
-
   $(window).on('scroll', function(){
   let scroll = $(window).scrollTop();
   console.log(scroll)
@@ -33,17 +53,14 @@ const Navbar = () =>{
     }
   });
 
-  const itemStyle = {
-    marginInline: "4vw",
-    fontSize: "1.2rem",
-  }
+  // const { hoverStyle, ...hoverProps } = useHover();
 
   return (
     <nav className="navbar" style={Object.assign(preStyle, navStyle)}>
-      <Link to="/" style={itemStyle}>Home</Link>
-      <Link to="/" style={itemStyle}>Projects</Link>
-      <Link to="/" style={itemStyle}>Photography</Link>
-      <Link to="/about" style={itemStyle}>About</Link>
+      <Link to="/" className="item">Home</Link>
+      <Link to="/" className="item">Projects</Link>
+      <Link to="/" className="item">Photography</Link>
+      <Link to="/about" className="item">About</Link>
     </nav>
   )
 }
